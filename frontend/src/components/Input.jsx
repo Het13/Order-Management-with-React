b1564d193040bfbb5135ccf8810f73d0a64acc95
Het Patel <het.patel@mobiuso.com>
2024-02-27 10:04:39 +0530

    add input component

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-
 function Input(props) {
-  return (
-    <div>
-      
-    </div>
-  )
+    const {register} = props
+    return (<>
+        <input
+            type={props.type}
+            className="form-control"
+            id={props.id}
+            placeholder={"name@example.com"}
+            {...register(props.name)}
+        />
+        <p className="pt-2" style={{"color": "red"}}>{props.errors?.message}</p>
+        <label htmlFor={props.id}>{props.title}</label>
+    </>)
 }
 
-export default Input
+export default Input;
