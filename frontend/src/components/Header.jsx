339746fb3bed5914a0e9cc89f39a2794b611362a
Het Patel <het.patel@mobiuso.com>
2024-02-28 10:01:18 +0530

    style header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,53 +1,69 @@
 import React from "react";
 import {Link, Outlet} from "react-router-dom";
 import {connect} from "react-redux";
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded';
+import PersonRoundedIcon from '@mui/icons-material/PersonRounded';
+import LoginRoundedIcon from '@mui/icons-material/LoginRounded';
+import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
+import FormatListBulletedRoundedIcon from '@mui/icons-material/FormatListBulletedRounded';
 
 function Header({user, cart}) {
     return (<>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
-                <Link className="navbar-brand" to="/">Order Management</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarNav"
-                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <Link className="nav-link " to='/'>Home</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to='/products'>Products</Link>
-                        </li>
-                        <li className="nav-item">
-                            {user.isAuthenticated ?
-                                <Link className="nav-link" to='/cart'>My Cart
-                                    <span
-                                        style={{
-                                            backgroundColor: "green", borderRadius: "50%", padding: "0 0.5rem"
-                                        }}>{cart.length}</span></Link> :
-                                <Link className="nav-link" to='/cart'>My Cart</Link>
+            <nav className="navbar navbar-expand-lg bg-secondary">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/">Order Management</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarNav"
+                            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav ms-auto">
+                            <li className="nav-item">
+                                <Link className="nav-link " to='/'><HomeRoundedIcon/></Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link "
+                                      to='/products'><FormatListBulletedRoundedIcon/></Link>
+                            </li>
+                            <li className="nav-item">
+                                {user.isAuthenticated ?
+                                    <Link className="nav-link " to='/cart'>
+                                        <span><ShoppingCartRoundedIcon/></span>
+                                        <span className="badge bg-success rounded-pill ms-1">{cart.length}</span>
+                                    </Link> :
+                                    <Link className="nav-link " to='/cart'><ShoppingCartRoundedIcon/></Link>
+                                }
+                            </li>
+                            {user.isAuthenticated
+                                ? <>
+                                    <li className="nav-item">
+                                        <Link className="nav-link  " to='/'><PersonRoundedIcon/></Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link className="nav-link  " to='/'><LogoutRoundedIcon/></Link>
+                                    </li>
+                                </> :
+                                <li className="nav-item">
+                                    <Link className="nav-link  " to='/login'><LoginRoundedIcon/></Link>
+                                </li>
                             }
 
-                        </li>
-                        <li className="nav-item">
-                            {user.isAuthenticated ?
-                                <Link className="nav-link disabled" to='/register'>Logged In</Link> :
-                                <Link className="nav-link " to='/register'>Login/Sign Up</Link>}
-
-                        </li>
-                    </ul>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
-        <Outlet/>
-    </>);
+            </nav>
+            <Outlet/>
+
+        </>
+    );
 }
 
 const mapStateToProps = (state) => ({
     cart: state.cart.cart,
-    user: state.user
+    user:
+    state.user
 })
 
 export default connect(mapStateToProps)(Header);
