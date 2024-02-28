c23650f5f4748f63ab4ccd2ea6d15fbff13987f5
Het Patel <het.patel@mobiuso.com>
2024-02-28 10:02:26 +0530

    change get_by_id api to return address

--- backend/customers/services/customer_services.py
+++ backend/customers/services/customer_services.py
@@ -72,21 +72,23 @@ def get_by_id(customer_id):
     database_cursor = connection.cursor()
 
     try:
-        query = 'SELECT * FROM ONLINE_CUSTOMER WHERE CUSTOMER_ID = %s'
+        query = 'SELECT * FROM ONLINE_CUSTOMER JOIN address ON online_customer.ADDRESS_ID = address.ADDRESS_ID WHERE CUSTOMER_ID = %s'
 
         database_cursor.execute(query, (customer_id,))
-        data = database_cursor.fetchone()
-        if data is None:
+        customer_data = database_cursor.fetchone()
+        if customer_data is None:
             raise NotFoundError
 
         attributes = ['id', 'first_name', 'last_name', 'email', 'phone', 'address_id', 'creation_date', 'username',
-                      'gender']
+                      'gender', 'address_id', 'address_line_1', 'address_line_2', 'city', 'state', 'pincode', 'country']
 
         customer = {}
-        for i, j in zip(attributes, data):
+        for i, j in zip(attributes, customer_data):
             if j is None:
                 continue
             customer[i] = j
+
+        print(customer)
         return customer
     except NotFoundError:
         raise NotFoundError
